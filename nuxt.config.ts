21c1aafa6eeaf9cbe1bbcd3c3260889f40c56167
Maxime Renaudin <m.renaudin@hotelbreak.com>
2026-07-31 21:30:47 +0200

    Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2026-07-31',
+
+  app: {
+    head: {
+      title: 'Culture Vault',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
+        // Configuration iOS (Ajout à l'écran d'accueil)
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+        { name: 'apple-mobile-web-app-title', content: 'Culture Vault' },
+        // Couleur de la barre d'adresse sur mobile (Thème sombre)
+        { name: 'theme-color', content: '#121212' }
+      ],
+      link: [
+        // Favicon standard
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        // Icône écran d'accueil iPhone / iPad
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
+      ]
+    }
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   modules: [
